0d19f7b58334144ee7fd3a425aa6bb04aa58f732
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-04-02 22:17:11 +0200

    Fix project environment (build and dev serve)

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
+export default defineConfig({
+  publicDir: resolve(__dirname, "public"),
+  base: "/",
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        shop: resolve(root, "shop", "index.html"),
+      },
+    },
+  },
+});
