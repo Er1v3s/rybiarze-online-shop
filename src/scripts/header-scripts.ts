76a86420d6841f94fe0d0aba003130d9883ae6d4
Filip Statkiewicz <stf6.dev@gmail.com>
2023-04-03 13:48:20 +0200

    Create video control function (pause when is not visible)

--- src/scripts/header-scripts.ts
+++ src/scripts/header-scripts.ts
@@ -1,3 +1,5 @@
+// Overlay
+
 const header_images: NodeListOf<HTMLDivElement> = document.querySelectorAll(
   ".commercial-baner div"
 )!;
@@ -13,8 +15,37 @@ const text_content: Array<string> = [
 header_images.forEach((element, index) => {
   const header_image_overlay: HTMLDivElement = document.createElement("div");
   header_image_overlay.setAttribute("class", "overlay");
+
   const overlay_text: HTMLSpanElement = document.createElement("span");
   overlay_text.innerText = text_content[index];
+
   header_image_overlay.appendChild(overlay_text);
+
   element.appendChild(header_image_overlay);
 });
+
+// Pause video when is not visible
+
+const video: HTMLVideoElement = document.querySelector("video")!;
+let isPaused: boolean = true;
+
+const handleVideo = (): void => {
+  const isVisible = isElementVisible(video);
+  if (isVisible && isPaused) {
+    video.play();
+    isPaused = false;
+  } else if (!isVisible && !isPaused) {
+    video.pause();
+    isPaused = true;
+  }
+};
+
+const isElementVisible = (element: HTMLElement): boolean => {
+  const rect = element.getBoundingClientRect();
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  return rect.bottom >= 0 && rect.top <= windowHeight;
+};
+
+document.addEventListener("scroll", handleVideo);
+document.addEventListener("visibilitychange", handleVideo);
